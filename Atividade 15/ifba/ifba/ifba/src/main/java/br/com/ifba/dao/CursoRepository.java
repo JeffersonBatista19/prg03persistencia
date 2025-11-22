e66bc1166e5b49de2c18dca39cfe1e1775ed47ba
JeffersonBatista19 <jeffersonbatista2003@gmail.com>
2025-11-22 20:42:34 -0300

    Subindo Implementeção de Spring (Atividade15)

---
+++ Atividade 15/ifba/ifba/ifba/src/main/java/br/com/ifba/dao/CursoRepository.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package br.com.ifba.dao;
+
+import br.com.ifba.entity.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+
+/**
+ *
+ * @author João P. Arquim
+ */
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long>{
+
+    public List<Curso> findByNome(String nome);
+}
